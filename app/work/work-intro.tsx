5430206e52df8d8a1ba5ce9ddaf090240e7aa341
robinxpfp <robinpme@proton.me>
2025-04-04 12:16:50 -0400

    remove bg and text

--- app/work/work-intro.tsx
+++ app/work/work-intro.tsx
@@ -1,11 +1,8 @@
 export function WorkIntro() {
   return (
-    <div className="mb-6">
-      <h2 className="font-bold text-foreground mb-4">Open Source</h2>
-      <p className="text-foreground">
-        I like to collaborate on open source projects and these are some of the
-        works I have done.
-      </p>
+    <div className="mb-6 text-foreground">
+      I like to collaborate on open source projects and these are some of the
+      works I have done.
     </div>
   );
 }
