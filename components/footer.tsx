a0124cce2ad2e2fd4219813c168524df233b6e44
Saul <83991228+devdotsaul@users.noreply.github.com>
2025-11-27 04:57:37 -0400

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="my-10 text-sm text-muted-foreground">
-      <div className="text-right">&copy; 2025, robinxpfp</div>
+      <div className="text-right">&copy; 2025, Saul Perez</div>
     </footer>
   );
 }
